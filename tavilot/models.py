b903f010b894496d6d92c097070a26dcee712811
abbosbek <hikmatullaevabbos24@gmail.com>
2024-10-29 07:40:50 -0400

    models fixed

--- tavilot/models.py
+++ tavilot/models.py
@@ -11,23 +11,23 @@ class Chapter(BaseModel):
         return self.name
 
     class Meta:
-        verbose_name = 'Глава'
-        verbose_name_plural = 'Главы'
+        verbose_name = 'Сура'
+        verbose_name_plural = 'Суры'
         ordering = ('-created_at',)
 
 class Verse(BaseModel):
-    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE, verbose_name='глава')
-    number = models.PositiveIntegerField(verbose_name="порядковый номер стиха")
-    text = models.TextField(verbose_name="текст")
-    text_arabic = models.TextField(verbose_name="текст на арабском языке")
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE, verbose_name='сура')
+    number = models.PositiveIntegerField(verbose_name="порядковый номер айат")
+    text = models.TextField(verbose_name="айат")
+    text_arabic = models.TextField(verbose_name="айат на арабском языке")
     description = models.TextField(verbose_name="описание")
 
     def __str__(self):
         return str(self.id) or ''
 
     class Meta:
-        verbose_name = 'Стих'
-        verbose_name_plural = 'Стихи'
+        verbose_name = 'Айат'
+        verbose_name_plural = 'Айаты'
         ordering = ('-created_at',)
 
 
@@ -47,7 +47,6 @@ class Sales(BaseModel):
 
 class AboutUs(BaseModel):
     description = models.TextField(verbose_name="описание")
-    app_updates = models.TextField(verbose_name="обновления приложений", blank=True, null=True)
 
     def __str__(self):
         return str(self.id) or ''
@@ -76,7 +75,7 @@ class Post(BaseModel):
     file = models.FileField(upload_to='to_students/', verbose_name="файл", null=True, blank=True)
     description = HTMLField(verbose_name='описание')
     is_published = models.BooleanField(default=False, verbose_name="опубликовано")
-    is_premium = models.BooleanField(default=False, verbose_name="это премиум")
+    is_premium = models.BooleanField(default=False, verbose_name="премиум")
 
     def __str__(self):
         return str(self.id) or ''
