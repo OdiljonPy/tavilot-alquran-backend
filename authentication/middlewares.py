88b8dbfb94e6e58e89a70f4996cde944170db2cb
Damir <nimadjonov.2404@gmail.com>
2024-11-01 15:11:47 +0500

    resolve bug in middleware

--- authentication/middlewares.py
+++ authentication/middlewares.py
@@ -1,17 +1,22 @@
 from django.http import JsonResponse
-from django.utils.deprecation import MiddlewareMixin
 from django.urls import reverse
+from django.utils.deprecation import MiddlewareMixin
 
 from utils.check_token import validate_token
 
 
 class AuthenticationBaseRedirectMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        exclude_target_urls = [
-            reverse('chapter_detail', kwargs = {'pk': 3}), reverse('auth_me')]
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        chapter_id = view_kwargs.get('pk')
+        exclude_target_urls = [reverse('auth_me')]
+        if chapter_id:
+            exclude_target_urls.append(reverse('chapter_detail', kwargs={'pk': chapter_id}))
 
-        if not request.path.startswith('/api/v1/') and request.path in exclude_target_urls:
+        if request.path.startswith('/api/v1/') and request.path in exclude_target_urls:
             payload = validate_token(request.headers.get('Authorization'))
 
             if payload is None:
                 return JsonResponse(data={'result': "", "error": "Unauthorized access", 'ok': False}, status=401)
+            return view_func(request, *view_args, **view_kwargs)
+
+        return None
