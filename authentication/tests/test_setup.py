3bbe28f8fe2d5c2029f518fae68352b022d07301
Damir <nimadjonov.2404@gmail.com>
2024-11-04 13:10:17 +0500

    wrote tests

--- authentication/tests/test_setup.py
+++ authentication/tests/test_setup.py
@@ -1,5 +1,7 @@
 from rest_framework.test import APITestCase, APIClient
 from django.urls import reverse
+import random
+import uuid
 
 from ..models import OTP, User
 
@@ -8,15 +10,27 @@ class TestSetUp(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.register_url = reverse('register')
+        self.verify_url = reverse('verify')
+        self.login_url = reverse('login')
 
         self.user_data = {
             'phone_number': '+998933787766',
             'password': '1221'
         }
 
+        self.user_not_valid_data ={
+            'phone_number': '+998933787777',
+            'password': '1221'
+        }
+
+        self.otp_data = {
+            'otp_code' : str(random.randint(100000, 999999)),
+            'otp_key' : uuid.uuid4()
+        }
+
         self.user = User.objects.create(phone_number=self.user_data['phone_number'], password=self.user_data['password'])
 
-        self.otp = OTP.objects.create(user=self.user)
+        self.otp = OTP.objects.create(user=self.user, otp_code=self.otp_data['otp_code'], otp_key=self.otp_data['otp_key'])
 
         return super().setUp()
 
--- authentication/tests/test_views.py
+++ authentication/tests/test_views.py
@@ -1,5 +1,6 @@
 from .test_setup import TestSetUp
 
+
 class TestViews(TestSetUp):
     def test_user_cannot_register_when_no_data(self):
         response = self.client.post(self.register_url)
@@ -7,11 +8,27 @@ class TestViews(TestSetUp):
         self.assertEqual(response.status_code, 400)
 
     def test_user_can_register_correctly(self):
-        response = self.client.post(self.register_url, self.user_data, format='json')
+        res = self.client.post(self.register_url, self.user_data, format='json')
 
+        self.assertIsNotNone(res.data['result'].get('otp_code'), 'OTP code should be generated and returned.')
+        self.assertIsNotNone(res.data['result'].get('otp_key'), 'OTP key should be generated and returned.')
+        self.assertEqual(res.status_code, 201)
 
-        self.assertIsNotNone(response.data['result'].get('otp_code'), 'OTP code should be generated and returned.')
-        self.assertIsNotNone(response.data['result'].get('otp_key'), 'OTP key should be generated and returned.')
-        self.assertEqual(response.status_code, 201)
+    def test_user_cannot_verify_without_otp(self):
+        res = self.client.post(self.verify_url)
 
-    def test_user_cannot_login_with_unverified(self):
+        self.assertEqual(res.status_code, 400)
+
+    def test_user_can_verify_with_otp(self):
+        res = self.client.post(self.verify_url, self.otp_data, format='json')
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.data.get('ok'), True)
+
+    def test_user_cannot_login_with_not_valid_data(self):
+        res = self.client.post(self.login_url, self.user_not_valid_data, format='json')
+        print(res.data)
+        print(res.status_code)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.data.get('ok'), False)
