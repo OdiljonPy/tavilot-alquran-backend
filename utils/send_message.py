9ee5402f7214d896d68db8f97c8658596fd24e1f
abbosbek <hikmatullaevabbos24@gmail.com>
2024-11-16 18:18:25 -0500

    Telegram chanel id and bot token added

--- utils/send_message.py
+++ utils/send_message.py
@@ -7,4 +7,4 @@ def send_telegram_otp_code(otp):
     message = """P/j: Tavilot-alquran\nPhone: {}\nOTP_code {}\nExpire in {}""".format(
         otp.user.phone_number, otp.otp_code, (otp.created_at + timedelta(minutes=3)).strftime('%H:%M:%S'))
 
-    requests.get(settings.TELEGRAM_API_URL.format(settings.BOT_TOKEN, message, settings.CHANNEL_ID))
+    requests.get(settings.TELEGRAM_API_URL.format(settings.TELEGRAM_BOT_TOKEN, message, settings.TELEGRAM_CHANNEL_ID))
